07ac79ffd153bbb46030497b2aec50d4a592b984
seeun <tpdms3139@naver.com>
2026-01-29 00:58:37 +0900

    Design/#7/로그인 페이지 구현

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,3 +1,25 @@
+import AuthLayout from '@/layouts/AuthLayout'
+import AuthButton from '@/components/Auth/AuthButton'
+import AuthCard from '@/components/Auth/AuthCard'
+import AuthInput from '@/components/Auth/AuthInput'
+import AuthTitle from '@/components/Auth/AuthTitle'
+import AuthForm from '@/components/Auth/AuthForm'
+import AuthFooterLink from '@/components/Auth/AuthFooterLink'
+
 export default function LoginPage() {
-  return <div></div>;
+  return (
+    <AuthLayout>
+      <AuthCard>
+        <AuthTitle title='LOGIN' subtitle='Welcome PingPong' />
+
+        <AuthForm>
+          <AuthInput placeholder='email' />
+          <AuthInput placeholder='password' type='password' />
+          <AuthButton>Start PingPong</AuthButton>
+        </AuthForm>
+
+        <AuthFooterLink text="Don't have an account?" linkText='Sign up' href='/signup' />
+      </AuthCard>
+    </AuthLayout>
+  )
 }
