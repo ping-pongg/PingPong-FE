6c7b8ab9e19b87eed5d546a2bddc364abcc8c478
seeun <tpdms3139@naver.com>
2026-01-29 00:58:50 +0900

    Design/#7/역할 카드 그림자 변경

--- src/components/Home/RoleCard.tsx
+++ src/components/Home/RoleCard.tsx
@@ -7,7 +7,7 @@ interface RoleCardProps {
 
 export default function RoleCard({ title, description }: RoleCardProps) {
   return (
-    <div className='group relative rounded-2xl bg-white p-4 shadow-base transition hover:-translate-y-1 hover:shadow-lg h-[50vh]'>
+    <div className='group relative rounded-2xl bg-white p-4 shadow-lg transition hover:-translate-y-1 hover:shadow-lg h-[50vh]'>
       <div className='h-full border border-gray-200 rounded-2xl p-4 flex flex-col items-center justify-center'>
         <h2 className='mb-4 text-2xl font-extrabold'>{title}</h2>
         <p className='mb-[20vh] text-xs text-gray-300 whitespace-pre-line text-center'>
