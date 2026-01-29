7da129df7b284414a95a2306e284b973930a8620
seeun <tpdms3139@naver.com>
2026-01-29 23:16:14 +0900

    Design/#9/Auth 레이아웃 위치 조정

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,3 +1,5 @@
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return <div className='min-h-screen w-full flex items-center justify-center'>{children}</div>
+  return (
+    <div className='min-h-screen w-full flex items-center justify-center pt-14'>{children}</div>
+  )
 }
