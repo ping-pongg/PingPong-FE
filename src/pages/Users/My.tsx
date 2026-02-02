aab006e204bdfe1f4274f9da6cf0a4f8e8db204d
Park Seeun <155715979+JMUNG1@users.noreply.github.com>
2026-02-03 02:23:39 +0900

    Design/#16/마이페이지 구현

---
+++ src/pages/Users/My.tsx
@@ -0,0 +1,11 @@
+import ProfileSection from "@/components/My/ProfileSection"
+import MyTeamSection from "@/components/My/MyTeamSection"
+
+export default function MyPage() {
+  return (
+    <div className="w-full pt-25">
+      <ProfileSection />
+      <MyTeamSection />
+    </div>
+  )
+}
