4cc0e204e72c8dcd0f02816b10bbe2188f582927
Park Seeun <155715979+JMUNG1@users.noreply.github.com>
2026-02-04 00:01:35 +0900

    Feat/#18/라우터 추가 및 변경

--- src/app/router/path.ts
+++ src/app/router/path.ts
@@ -1,8 +1,13 @@
 export const PATH = {
-  home: "/",
-  login: "/login",
-  signup: "/signup",
-  users: "/users",
-  my: "/mypage",
-  userDetail: (id: string | number) => `/users/${id}`,
-} as const;
+  home: '/',
+  login: '/login',
+  signup: '/signup',
+  users: '/users',
+  my: '/mypage',
+  team: {
+    create: '/team/create',
+    detail: (teamId: string | number) => `/team/${teamId}`,
+    developer: (teamId: string | number, userId: string | number) =>
+      `/team/${teamId}/developer/${userId}`,
+  },
+} as const
