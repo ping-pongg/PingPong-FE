e8e56375bc6bebe458c6cdcbdd3b8d6b0041759e
Park Seeun <155715979+JMUNG1@users.noreply.github.com>
2026-02-27 00:53:34 +0900

    Design/#53/라인 겹침 제거

--- src/components/PM/Gantt/GridLines.tsx
+++ src/components/PM/Gantt/GridLines.tsx
@@ -3,13 +3,13 @@ import { TASK_COL_WIDTH } from '@/utils/date'
 export default function GridLines({ DAY_WIDTH, numDays }: { DAY_WIDTH: number; numDays: number }) {
   return (
     <div
-      className='absolute top-0 bottom-0 right-0 flex pointer-events-none z-0'
+      className='absolute top-0 bottom-0 right-0 flex pointer-events-none z-0 pl-20'
       style={{ left: TASK_COL_WIDTH }}
     >
       {[...Array(numDays)].map((_, i) => (
         <div
           key={i}
-          className='shrink-0 border-l border-gray-100 h-full'
+          className={`shrink-0 border-l border-gray-100 h-full`}
           style={{ width: DAY_WIDTH }}
         />
       ))}
