bbc89a4a9e6dafb4a0ba86052b8d5337fdd5f23e
Park Seeun <155715979+JMUNG1@users.noreply.github.com>
2026-02-27 00:54:16 +0900

    Chore/#53/padding top값 조정

--- src/pages/Users/MyPage.tsx
+++ src/pages/Users/MyPage.tsx
@@ -1,9 +1,9 @@
-import ProfileSection from "@/components/My/ProfileSection"
-import MyTeamSection from "@/components/My/MyTeamSection"
+import ProfileSection from '@/components/My/ProfileSection'
+import MyTeamSection from '@/components/My/MyTeamSection'
 
 export default function MyPage() {
   return (
-    <div className="w-full pt-25">
+    <div className='w-full pt-20'>
       <ProfileSection />
       <MyTeamSection />
     </div>
